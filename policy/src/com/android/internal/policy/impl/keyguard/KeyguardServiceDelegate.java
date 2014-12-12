00159d5d2b844d278aa3c3c4978d73182cab2550
Chet Kener <Cl3Kener@gmail.com>
2014-12-12 01:20:48 -0500

    Disable Lots of Debugging
    Revert "Debugging: Enable All Debugging until GPS bug is Squashed"
    
    This reverts commit a7e2e0bfc70c866645c2f45df2ab06030c7e1131.
    
    Change-Id: I9836dd477009ece01dda6a4feaaf00d1f7a3bfd5
    From: Optipop rom

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardServiceDelegate.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardServiceDelegate.java
@@ -37,7 +37,7 @@ public class KeyguardServiceDelegate {
     private static final String EXTRA_ACTIVE = "active";
 
     private static final String TAG = "KeyguardServiceDelegate";
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
     protected KeyguardServiceWrapper mKeyguardService;
     private View mScrim; // shown if keyguard crashes
     private KeyguardState mKeyguardState = new KeyguardState();
